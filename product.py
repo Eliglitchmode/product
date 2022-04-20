6d0f02e1c7b318d2439cf4c0817c64b4989f3aaf
Eli <yuchien.elliot@gmail.com>
2022-04-19 17:00:51 -0700

    final version

--- product.py
+++ product.py
@@ -1,11 +1,21 @@
+# Read File
+products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if 'Product, Price' in line:
+			continue #Jump to next loop and skip the current one 
+		name, price = line.strip().split(',')
+		products.append([name,price])
+print(products)
+
 #Two-dimensional list
 
-products = []
 while True:
 	name = input('Please enter the name of the product: ')
 	if name == 'q':  #exit
 		break
 	price = input('Please enter the price: ')
+	price = int(price)
 #	p = []
 #	p.append(name)
 #	p.append(price)
@@ -14,4 +24,11 @@ while True:
 	products.append([name, price])
 print(products)
 
-print(products[0][0])
+for p in products:
+	print('The price of ',p[0],'is ', p[1])
+
+# Use encoding='utf-8' can prevent language not showing properly
+with open('products.csv', 'w', encoding='utf-8') as f:   #with --> help auto-close the file after written
+	f.write('Product, Price\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
