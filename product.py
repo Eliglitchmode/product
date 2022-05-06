05f0a3a6e3ef40fbdc45ac6ceb491b340a17de07
Eli <yuchien.elliot@gmail.com>
2022-05-05 20:30:34 -0700

    Add check file exist

--- product.py
+++ product.py
@@ -1,15 +1,21 @@
+import os # operating system
 # Read File
 products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if 'Product, Price' in line:
-			continue #Jump to next loop and skip the current one 
-		name, price = line.strip().split(',')
-		products.append([name,price])
-print(products)
+if os.path.isfile('products.csv'): #Check if the file is existing
+	print('yeah! I found it!')
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if 'Product, Price' in line:
+				continue #Jump to next loop and skip the current one 
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+
+else:
+	print('Not found....')
 
 #Two-dimensional list
-
+#Let user input the price & product
 while True:
 	name = input('Please enter the name of the product: ')
 	if name == 'q':  #exit
@@ -24,9 +30,11 @@ while True:
 	products.append([name, price])
 print(products)
 
+#Print all purchases
 for p in products:
 	print('The price of ',p[0],'is ', p[1])
 
+#Write into the file
 # Use encoding='utf-8' can prevent language not showing properly
 with open('products.csv', 'w', encoding='utf-8') as f:   #with --> help auto-close the file after written
 	f.write('Product, Price\n')
