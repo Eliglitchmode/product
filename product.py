d3c05977078656178c4409e28d63151572d6e9a9
Eli <yuchien.elliot@gmail.com>
2022-05-10 00:25:10 -0700

    Refactor

--- product.py
+++ product.py
@@ -1,42 +1,59 @@
 import os # operating system
-# Read File
-products = []
-if os.path.isfile('products.csv'): #Check if the file is existing
-	print('yeah! I found it!')
-	with open('products.csv', 'r', encoding = 'utf-8') as f:
-		for line in f:
-			if 'Product, Price' in line:
-				continue #Jump to next loop and skip the current one 
-			name, price = line.strip().split(',')
-			products.append([name, price])
-	print(products)
 
-else:
-	print('Not found....')
+
+def read_file(filename):
+    products = []
+    with open(filename, 'r', encoding = 'utf-8') as f:
+        for line in f:
+            if 'Product, Price' in line:
+                continue #Jump to next loop and skip the current one 
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    return products
+
 
 #Two-dimensional list
 #Let user input the price & product
-while True:
-	name = input('Please enter the name of the product: ')
-	if name == 'q':  #exit
-		break
-	price = input('Please enter the price: ')
-	price = int(price)
-#	p = []
-#	p.append(name)
-#	p.append(price)
-# 	can be written as follows:
-#	p = [name, price]
-	products.append([name, price])
-print(products)
+def user_input(products):
+    while True:
+        name = input('Please enter the name of the product: ')
+        if name == 'q':  #exit
+            break
+        price = input('Please enter the price: ')
+        price = int(price)
+    #   p = []
+    #   p.append(name)
+    #   p.append(price)
+    #   can be written as follows:
+    #   p = [name, price]
+        products.append([name, price])
+    print(products)
+    return products
 
 #Print all purchases
-for p in products:
-	print('The price of ',p[0],'is ', p[1])
+def print_products(products):
+    for p in products:
+        print('The price of ',p[0],'is ', p[1])
 
 #Write into the file
 # Use encoding='utf-8' can prevent language not showing properly
-with open('products.csv', 'w', encoding='utf-8') as f:   #with --> help auto-close the file after written
-	f.write('Product, Price\n')
-	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n')
+def write_file(filename, products):
+    with open(filename, 'w', encoding='utf-8') as f:   #with --> help auto-close the file after written
+        f.write('Product, Price\n')
+        for p in products:
+            f.write(p[0] + ',' + str(p[1]) + '\n')
+
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename):
+        products = read_file(filename)
+        print('yeah! I found it!')
+    else: 
+        print('Not found....')
+
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+
+main()
